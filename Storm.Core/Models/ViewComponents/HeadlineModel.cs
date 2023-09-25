22eed8f7dcf5ca75d906c80bdb482fc8f0f83919
louisferreira <louisferreira@hotmail.com>
2023-09-25 07:00:10 +0100

    added dictionary values

---
+++ Storm.Core/Models/ViewComponents/HeadlineModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm.Core.Models.ViewComponents
+{
+    public class HeadlineModel
+    {
+        public string Headline { get; set; }
+        public string HeadlineStyle { get; set; }
+    }
+}
